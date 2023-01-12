6c9df60d4dcc855f2a64685d44329c8855cfe578
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2023-01-12 21:40:53 +0100

    wegwergewr

--- src/main/java/arraybidimensionales/ejercicio19.java
+++ src/main/java/arraybidimensionales/ejercicio19.java
@@ -56,15 +56,51 @@ public class ejercicio19 {
                     fila = teclado.nextInt();
 
                     System.out.println("Dime un asiento de la fila: " + fila);
-                    fila = teclado.nextInt();
+                    asiento = teclado.nextInt();
 
-                    
+                    if(asientos[fila][asiento] == false) {
+                        
+                        System.out.println("Ese asiento esta ocupado");
+                        System.out.println("");
+                        
+                        
+                        
+                    } else {
+                        
+                        asientos[fila][asiento] = false;
+                        System.out.println("Su asiento se ha reservado correctamente");
+                        
+                        System.out.println("");
+                    }
 
                     break;
 
                 case 2:
 
                     System.out.println("Ha elegido cancelar un asiento");
+                    System.out.println("");
+                    
+                    System.out.println("Dime una fila");
+                    fila = teclado.nextInt();
+
+                    System.out.println("Dime un asiento de la fila: " + fila);
+                    asiento = teclado.nextInt();
+                    
+                    if(asientos[fila][asiento] == true) {
+                        
+                        System.out.println("Ese asiento no esta reservado");
+                        
+                        System.out.println("");
+                        
+                    } else {
+                        
+                        asientos[fila][asiento] = true;
+                        System.out.println("Se ha cancelado correctamente su asiento");
+                        
+                        System.out.println("");
+                        
+                    }
+                    
 
                     break;
 
